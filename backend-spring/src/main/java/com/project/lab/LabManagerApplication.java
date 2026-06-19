d4d3c51e4c87764f7fd6d1bd60094bdf773da61e
LORENZOCIRCELLI <lorenzocircelli5@gmail.com>
2026-06-18 22:16:39 -0300

    Backend Spring Project Created

---
+++ backend-spring/src/main/java/com/project/lab/LabManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabManagerApplication.class, args);
+	}
+
+}
